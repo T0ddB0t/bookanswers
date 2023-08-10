c10da177ac2b199b10e77e9b8037d1d29958879e
frak <upupou@gmail.com>
2023-08-09 21:33:39 -0400

    updated Fraction.h for >> and << operator

--- 1.12/Fraction.h
+++ 1.12/Fraction.h
@@ -1,5 +1,22 @@
 #include <iostream>
 
+using namespace std;
 class Fraction{
-    //Branched!!
+protected:
+    ostream writeFraction(ostream&);
+    istream readFraction(istream&);
+    friend ostream& operator<<(ostream& out, Fraction& fr){
+        return fr.writeFraction(out);
+    }
+    friend istream& operator>>(istream& in, Fraction& fr){
+        return fr.readFraction(in);
+    }
+private:
+    double value;
+public:
+    Fraction(double val) : value(val){}
+    friend Fraction operator/(const Fraction& numerator, const Fraction& dominator){
+        
+    }
+
 };
