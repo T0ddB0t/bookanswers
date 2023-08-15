d140fad3408424cb09969966b59ea33edca721fc
frak <upupou@gmail.com>
2023-08-15 14:27:19 -0400

    should be all thats needed

--- 1.12/Fraction.h
+++ 1.12/Fraction.h
@@ -5,26 +5,30 @@ class Fraction{
 private:
     int numerator;
     int denominator;
+    int factor;
+
 public:
     Fraction(int num, int den) : numerator(num), denominator(den) {}
+    Fraction(int fac, int num, int den) : factor(fac), numerator(num), denominator(den) {} 
+    ~Fraction(){}
     Fraction reduce() const {
         int newNumerator = numerator;
-        int factor = 0;
-        while(newNumerator >= denominator){
+        int newFactor = factor;
+        while(newNumerator > denominator){
             newNumerator -= denominator;
-            factor++;
+            newFactor++;
         }
-        return Fraction(newNumerator, denominator);
+        return Fraction(newFactor, newNumerator, denominator);
     }
     friend ostream& operator<<(ostream& out, const Fraction& fr){
-        Fraction reduced = fr.reduce();
-        out << reduced.numerator << reduced.denominator;
+        Fraction r = fr.reduce();
+        out << r.factor << " " << r.numerator << "/" << r.denominator;
         return out;
     }
     Fraction operator/(const Fraction& other) const {
         int n = numerator * other.denominator;
         int d = denominator * other.numerator;
-
+        
         return Fraction(n, d);
     }
     Fraction operator*(const Fraction& other) const {
@@ -32,5 +36,17 @@ public:
         int d = denominator / other.numerator;
 
         return Fraction(n, d);        
+    }
+    Fraction operator+(const Fraction& other) const {
+        int n = (numerator * other.denominator) + (other.numerator * denominator);
+        int d = denominator * other.denominator;
+
+        return Fraction(n,d);
+    }
+    Fraction operator-(const Fraction& other) const {
+        int n = (numerator * other.denominator) - (other.numerator * denominator);
+        int d = denominator * other.denominator;
+
+        return Fraction(n,d);
     }
 };
--- 1.12/fMain.cc
+++ 1.12/fMain.cc
@@ -1,7 +1,11 @@
 #include "Fraction.h"
 
 int main(){
-    Fraction frac(3, 2);
-    Fraction frac2(3, 7);
-    std::cout << frac << std::endl;
+    Fraction frac1(3, 2);
+    Fraction frac2(7, 3);
+    Fraction frac4(1,4,5);
+    Fraction frac3 = frac1 / frac2;
+    Fraction frac5 = frac1 - frac2;
+    //3/2 + 7/3
+    std::cout << frac5 << std::endl;
 }
